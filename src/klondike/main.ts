e877bfd2116b4b404a4f2543a7c722ea93334044
duncsully <duncsully@gmail.com>
2024-03-08 15:23:07 -0800

    fix: remove history router while on GH pages

--- src/klondike/main.ts
+++ src/klondike/main.ts
@@ -1,17 +1,4 @@
 import { render } from 'lit'
 import { Klondike } from './Klondike'
-import { html } from '../html'
-import { Routes } from '../Routes'
-import { Test } from '../Test'
 
-render(
-  html`
-    ${Routes({
-      '': () => html`<a href="/klondike">Klondike Solitaire</a>
-        <a href="/test">Testing</a>`,
-      klondike: Klondike,
-      test: Test,
-    })}
-  `,
-  document.body
-)
+render(Klondike(), document.body)
