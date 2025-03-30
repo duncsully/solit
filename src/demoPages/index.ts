f1e1948c4be7652c90a3a884a462a6e7ec3c4e45
duncsully <duncsully@gmail.com>
2025-03-29 21:02:03 -0700

    fix: set base path to get routing working

--- src/demoPages/index.ts
+++ src/demoPages/index.ts
@@ -2,16 +2,20 @@ import { render } from 'lit-html'
 import { html } from '../html'
 import { Router, setupHistoryRouting } from '../Routes'
 import { Klondike } from './klondike/Klondike'
+import viteConfig from '../../vite-site.config'
 
-setupHistoryRouting()
+const routes = () => {
+  setupHistoryRouting({ base: viteConfig.base })
 
-const routes = () =>
-  html`${Router({
-    '/': () => html`<h1>Games!</h1>
-      <nav style="display: flex; gap: 16px;">
-        <a href="/klondike">Klondike</a>
-      </nav>`,
-    '/klondike': Klondike,
-  })}`
+  return html`
+    ${Router({
+      '/': () => html`<h1>Games!</h1>
+        <nav style="display: flex; gap: 16px;">
+          <a href="/klondike">Klondike</a>
+        </nav>`,
+      '/klondike': Klondike,
+    })}
+  `
+}
 
 render(routes(), document.body)
